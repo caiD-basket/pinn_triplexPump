ffc83373ea11dd6021f5e5c9349c725b830549af
caiD-basket <caid0612@163.com>
2025-04-16 16:55:25 +0800

    Update demoFunc.py
    简单数据处理

--- demoFunc.py
+++ demoFunc.py
@@ -1,6 +1,7 @@
 import numpy as np
+import torch
 from torch import nn
-
+import pandas as pd
 
 
 class My_loss(nn.Module):
@@ -84,5 +85,53 @@ class My_loss(nn.Module):
             drho_mix_dp = rho_mix_init * numerator / (denominator ** 2)
 
         return drho_mix_dp
-    def forward(self,t0,p0,):
+    def forward(self,t0,p0,t,rho,V,m,p,pout, bulk_modulus_model, air_dissolution_model,
+                                   rho_L_atm, beta_L_atm, beta_gain, air_fraction,
+                                   rho_g_atm, polytropic_index, p_atm, p_crit, p_min):
+        theta =0.1
+        dpdt = (pout-p0)/(t-t0)
+        loss_physics = V * self.mixture_density_derivative(p, bulk_modulus_model, air_dissolution_model,
+                                                           rho_L_atm, beta_L_atm, beta_gain, air_fraction,
+                                                           rho_g_atm, polytropic_index, p_atm, p_crit, p_min)*dpdt-m/rho
+        loss_physics = abs(loss_physics)
+        loss_M = abs(pout-p)
+        loss = loss_M + theta*loss_physics
+        return loss
 #TODO 确定密度与压力的梯度函数，以及函数里面的参数
+data = pd.read_csv('PumpData4.csv')
+x = data.drop(['pOut','fault'],axis=1)
+y = data['pOut']
+train_size = int(len(x)*0.8)
+X_train = x.iloc[:train_size]
+y_train = y.iloc[:train_size]
+X_test = x.iloc[train_size:]
+y_test = y.iloc[train_size:]
+
+# 将数据转换为 torch.Tensor 格式
+X_train_tensor = torch.tensor(X_train.values, dtype=torch.float32)
+y_train_tensor = torch.tensor(y_train.values, dtype=torch.float32).unsqueeze(1)
+X_test_tensor = torch.tensor(X_test.values, dtype=torch.float32)
+y_test_tensor = torch.tensor(y_test.values, dtype=torch.float32).unsqueeze(1)
+print(X_test_tensor.shape)
+# print(y_test_tensor)
+# print(X_test_tensor)
+# print(y_test_tensor)
+class MyNeuralNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.input_to_hidden_layer = nn.Linear(5,8)
+        self.hidden_layer_activation = nn.ReLU()
+        self.hidden_to_output_layer = nn.Linear(8,1)
+    def forward(self, x):
+        x = self.input_to_hidden_layer(x)
+        x = self.hidden_layer_activation(x)
+        x = self.hidden_to_output_layer(x)
+        return x
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+mynet = MyNeuralNet().to(device)
+loss_func = nn.MSELoss()
+_Y = mynet(x)
+loss_value = loss_func(_Y,Y)
+print(loss_value)
+# tensor(127.4498, device='cuda:0', grad_fn=<MseLossBackward>)
+
